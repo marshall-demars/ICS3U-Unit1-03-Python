5d8aaca2836a361485b457f38de0ca9502e476c2
Marshall Demars <marshall.demars@mths.ca>
2022-09-15 15:16:41 +0000

    new file py

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Marshall Demars
+# Created on: Sept 2022
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+
+    print("Hello, World!")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
